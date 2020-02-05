86950c41a172b666368441692d9c73ba4ae43949
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-02-05 20:35:44 +0100

    (Front): Setup Quasar lib

---
+++ front/src/components/Weights.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    <ul v-for="(weight, index) in weights" v-bind:key="index">
+      <li>{{ weight.value }}</li>
+    </ul>
+  </q-page>
+</template>
+
+<style></style>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'Weights',
+  data() {
+    return {
+      weights: [],
+    };
+  },
+  methods: {
+    async getWeights() {
+      const response = await axios.get(
+        'http://localhost:6789/users/5e33f8ff97e8a70ec123e31a/weights',
+      );
+      this.weights = response.data;
+    },
+  },
+  mounted() {
+    this.getWeights();
+  },
+};
+</script>
