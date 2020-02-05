86604b878f83a29339853975a594f9f22778fa18
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-02-05 20:24:25 +0100

    (Front): Get data from backend

--- server.js
+++ server.js
@@ -5,12 +5,27 @@ const PORT = process.env.PORT || 6789;
 
 const mongoose = require('mongoose');
 
+const { DATABASE_URI, FRONT_APP_URI } = process.env;
+
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true });
+mongoose.connect(DATABASE_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 db.once('open', () => console.log('Connected to the database'));
+
+const corsConfig = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', FRONT_APP_URI);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  next();
+};
+
+app.use(corsConfig);
 
 const usersRouter = require('./routes/user');
 app.use('/', usersRouter);
