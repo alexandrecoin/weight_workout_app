547c01711b3345cf2de8cc7d4bbb581b7b88e34b
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-02-25 19:44:33 +0100

    (Back): Added authorize function

--- helpers/authorize.js
+++ helpers/authorize.js
@@ -1,1 +1,27 @@
-// Add session token first
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+// const VerifyToken = require('../helpers/verify-token'); Copied in the below function, to be imported
+const User = require('../models/User');
+// TODO : Create route rights
+const routesRights = require('../utils/routes-rights');
+const session = require('express-session');
+
+function isAuthorized(req, res, next) {
+  if (!session.token)
+    return res.status(403).json({ auth: false, message: 'No token provided' });
+
+  jwt.verify(session.token, process.env.TOKEN_SECRET_KEY, async (err, decoded) => {
+    if (err)
+      res
+        .status(500)
+        .send({ auth: false, message: 'Failed to authenticate token.' });
+
+    req.userId = decoded.id;
+    const user = await User.findById(req.userId);
+    if (!routesRights[req.route.path].includes(user.role))
+      return (res.error = res.status(401).json({ message: 'Unauthorized' }));
+    next();
+  });
+}
+
+module.exports = isAuthorized;
